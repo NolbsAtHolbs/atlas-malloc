4d7bf570887c94c5d6cb1e7e60539c232ca8ce66
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-09-18 18:52:17 -0500

    no struct

--- naive_malloc.c
+++ naive_malloc.c
@@ -9,7 +9,25 @@
  */
 void *naive_malloc(size_t size)
 {
-	size_t qpu_aligned = (((size + 7) / 8) * 8);
+	size_t qpu_aligned = (((size + 7) / 8) * 8); /* aligns size to 8 bytes*/
+	size_t total_size = qpu_aligned + sizeof(size_t); /* include space for header */
+	size_t page_size = sysconf(_SC_PAGESIZE); /* gets default system page size */
+	size_t num_pages = ((total_size + page_size - 1) / page_size); /* calculate required pages */
+	size_t alloc_size = num_pages * page_size; /* total allocation size */
 
-	return;
+	void *current_brk = sbrk(0); /* get current program break */
+	void *allocated_memory = sbrk(alloc_size); /* extend program break */
+
+	if (allocated_memory == (void *)-1) /* sbrk allocation failure */
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+
+	/* store the aligned size in the header to assist with memory management */
+	*((size_t *)allocated_memory) = qpu_aligned;
+
+	/* return a pointer to user-accessible memory area, after block size hdr */
+	return (void *)((char *)allocated_memory + sizeof(size_t));
+
 }
