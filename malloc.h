86ebc8a36a3a90a7f293deaf6ea82d40cfc1edb0
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-09-18 21:59:50 -0500

    betty

--- malloc.h
+++ malloc.h
@@ -14,7 +14,7 @@ void _free(void *ptr);
 
 /**
  * data_block_t - typedef for struct data_block
- * 
+ *
  * makes an alias for `struct data_block` called `data_block_t`.
  * allows for convenient reference to the struct type.
  */
@@ -25,7 +25,7 @@ typedef struct data_block data_block_t;
  * @size: size of the data block in bytes
  * @free_flag: flag indicating if the block is free or allocated
  * @next: ptr to next data block in linked list
- * 
+ *
  * this structure is used to manage memory blocks for _malloc.
  * blocks contain info about the size, status, and a ptr to the next block,
  * so it is a linked list of memory blocks.
