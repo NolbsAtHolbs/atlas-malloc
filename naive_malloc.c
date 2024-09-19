f1e0e9dab7391bb48f037cfd19dcf17460b43b76
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-09-18 19:29:29 -0500

    keeping track of brks and heaps

--- naive_malloc.c
+++ naive_malloc.c
@@ -15,8 +15,6 @@ void *naive_malloc(size_t size)
 	size_t qpu_aligned = (((size + 7) / 8) * 8); /* aligns size to 8 bytes*/
 	size_t total_size = qpu_aligned + sizeof(size_t); /* include space for header */
 	size_t page_size = sysconf(_SC_PAGESIZE); /* gets default system page size */
-	size_t num_pages = ((total_size + page_size - 1) / page_size); /* calculate required pages */
-	size_t alloc_size = num_pages * page_size; /* total allocation size */
 
 	if (current_brk == NULL)
 	{
