f79b4eaf906ba1ca231086ce937be8d148645781
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-09-18 22:12:35 -0500

    mark blocks as free

--- free.c
+++ free.c
@@ -1,13 +1,19 @@
 #include "malloc.h"
 
+extern data_block_t *global_free_list;
+
 /**
  * _free - frees the memory space pointed to by
  * @ptr: pointer to the memory space to be freed
  */
 void _free(void *ptr)
 {
+	data_block_t *block;
+
 	if (ptr == NULL)
-	{
 		return;
-	}
+
+	block = (data_block_t *)ptr - 1;
+
+	block->free_flag = 1;
 }
